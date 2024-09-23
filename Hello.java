38d2c6ef5ed38a2bd77a87c0effd4ecdfc8b7a83
Tapkir Pratiksha Balu <89315015+Pratikshatapkir@users.noreply.github.com>
2024-09-23 17:10:44 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,17 @@
+class Hello{
+public static void main(String args[]){
+
+   int num[]=new int[4];
+   num[0]=4;
+   num[1]=3;
+   num[2]=2;
+   num[3]=5;
+
+   for(int i=0;i<4;i++){
+    System.out.println(num[i]);
+   }
+
+   
+  }
+
+}
