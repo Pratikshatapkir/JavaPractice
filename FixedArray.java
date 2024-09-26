903e2659a7f8becef6211e9253f5c8a5c8c0339d
Tapkir Pratiksha Balu <89315015+Pratikshatapkir@users.noreply.github.com>
2024-09-27 00:22:06 +0530

    Add files via upload
    Fixed size array

---
+++ FixedArray.java
@@ -0,0 +1,16 @@
+public class FixedArray {
+    public static void main(String[] args) {
+         
+int num[]=new int[4];
+num[0]=1;
+num[1]=3;
+num[2]=6;
+num[3]=7;
+
+for(int i=0;i<num.length;i++){
+    System.out.println(num[i]);
+}
+
+    }
+    
+}
